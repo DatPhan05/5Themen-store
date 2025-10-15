fbbb8175272be282f0cc2833fca6f658bb90a74c
TBi_ets <134737984+DatPhan05@users.noreply.github.com>
2025-10-15 14:40:04 +0700

    Delete Giaodien directory

--- Giaodien/js/script.js
+++
@@ -1,8 +0,0 @@
-const top = document.querySelector(".top")
-window.addEventListener("scroll",function(){
-    const X = this.pageYOffset;
-    if(X>1){top.classList.add("active")}
-    else {
-        top.classList.remove("active")
-    }
-})
