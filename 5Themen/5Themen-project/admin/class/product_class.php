605aabf0a598fe7c340682f0aaf3bb287888bbe9
NNgDuy <dp1.1a7.ngocduy@gmail.com>
2025-11-03 20:56:37 +0700

    add condition upload file png, jpg, jpeg

--- 5Themen/5Themen-project/admin/class/product_class.php
+++ 5Themen/5Themen-project/admin/class/product_class.php
@@ -23,6 +23,11 @@ public function show_brand(){
     $result = $this->db->select($query);
     return $result;
 }
+public function show_brand_ajax($cartegory_id) {
+    $query = "SELECT * FROM tbl_brand WHERE cartegory_id = '$cartegory_id'";
+    $result = $this->db->select($query);
+    return $result;
+}
 public function insert_product(){
     $product_name = $_POST['product_name'];
     $cartegory_id = $_POST['cartegory_id'];
@@ -31,8 +36,26 @@ public function insert_product(){
     $product_price_new = $_POST['product_price_new'];
     $product_desc = $_POST['product_desc'];
     $product_img = $_FILES['product_img']['name'];
-    move_uploaded_file($_FILES['product_img']['tmp_name'], "uploads/".$_FILES['product_img']['name']);
-    $query = "INSERT INTO tbl_product (
+    $filetarget = basename($_FILES['product_img']['name']);
+    $filetype = strtolower(pathinfo($product_img, PATHINFO_EXTENSION));
+    $filesize = $_FILES['product_img']['size'];
+    if (file_exists("uploads/".$filetarget)) {
+        $alert = " File đã tồn tại";
+        return $alert;
+    }
+    else {
+        if($filetype != "jpg" && $filetype != "png" && $filetype != "jpeg") {
+            $alert = " Chỉ chọn file jpg, png, jpeg ";
+            return $alert;
+        }
+        else (
+            if ($filesize > 1000000){
+                $alert = " File không được lớn hơn 1MB ";
+                return $alert;
+            }
+            else {
+                move_uploaded_file($_FILES['product_img']['tmp_name'], "uploads/".$_FILES['product_img']['name']);
+                $query = "INSERT INTO tbl_product (
                 product_name,
                 cartegory_id,
                 brand_id,
@@ -48,20 +71,27 @@ public function insert_product(){
                 '$product_price_new',
                 '$product_desc',
                 '$product_img')";
-    $result = $this->db->insert($query);
-    if($result){
-        $query = "SELECT * FROM tbl_product ORDER BY product_id DESC LIMIT 1";
-        $result = $this->db->select($query)->fetch_assoc();
-        $product_di = $result['product_id'];
-        $filename = $_FILES['product_img_desc']['name'];
-        $filetmp = $_FILES['product_img_desc']['tmp_name'];
+                $result = $this->db->insert($query);
+                if($result){
+                    $query = "SELECT * FROM tbl_product ORDER BY product_id DESC LIMIT 1";
+                    $result = $this->db->select($query)->fetch_assoc();
+                    $product_di = $result['product_id'];
+                    $filename = $_FILES['product_img_desc']['name'];
+                    $filetmp = $_FILES['product_img_desc']['tmp_name'];
 
-        foreach ($filename as $key => $value) {
-            move_uploaded_file( $filetmp[$key], "uploads/".$value);
-            $query = "INSERT INTO tbl_product_img_desc (product_id, product_img_desc) VALUES ('$product_di', '$value')";
-            $result = $this->db->insert($query);
-        }
+                    foreach ($filename as $key => $value) {
+                        move_uploaded_file( $filetmp[$key], "uploads/".$value);
+                        $query = "INSERT INTO tbl_product_img_desc (product_id, product_img_desc) VALUES ('$product_di', '$value')";
+                        $result = $this->db->insert($query);
+                    }
+                }
+            }
+            
+
+        )
+       
     }
+    
     //header('Location:brandlist.php');
     return $result;
 }
